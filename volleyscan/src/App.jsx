6e8e6007ebb6fe72fd37c5504a853bd181cd3d7a
duvanalejandrosaaved@gmail.com <duvanalejandrosaaved@gmail.com>
2026-06-09 14:58:15 -0500

    se suben errores por arreglar mamahuevoooo

--- volleyscan/src/App.jsx
+++ volleyscan/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import UsuarioLayout from './layout/UsuarioLayout'
 import EntrenadorLayout from './layout/EntrenadorLayout'
-import Dashboard from './pages/Dashboard/Dashboard'
 import Perfil from './pages/panel_usuario/Perfil'
 import Rutinas from './pages/panel_usuario/Rutinas'
 import Historial from './pages/panel_usuario/Historial'
@@ -14,54 +13,50 @@ import Home from './pages/Home'
 import Login from './pages/login/Login'
 import Recuperacion from './assets/Auth/Recuperacion/Recuperacion'
 import Registro from './assets/Auth/Registro/Registro'
-import Dashboard from './pages/panel_entrenador/Dashboard'
-
-
+import DashboardEntrenador from './pages/panel_entrenador/DashboardEntrenador'
+import DashboardAdmin from './pages/Dashboard/DashboardAdmin'
+import Gestion from './pages/panel_entrenador/Gestion'
+import Horario from './pages/panel_entrenador/Horario'
+import Estadisticas from './pages/panel_entrenador/Estadisticas'
+import Reportes from './pages/panel_entrenador/Reportes'
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        
         <Routes>
-          {/* Página de inicio → Login */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/recuperacion" element={<Recuperacion />} />
-          <Route path="/registro"  element={<Registro />} />
+          <Route path="/registro" element={<Registro />} />
 
-
-
-          {/* Panel usuario (con sidebar) */}
-          <Route path='/usuario' element={<UsuarioLayout/>}>
-            <Route index element={<Dashboard />}/>
-            <Route path='perfil' element={<Perfil/>}/>
-            <Route path='rutinas' element={<Rutinas/>}/>
-            <Route path='historial' element={<Historial/>}/>
-            <Route path='contenido' element={<Contenido/>}/>
-            <Route path='progreso' element={<Progreso/>}/>
-            <Route path='analisis' element={<Analisis/>}/>
-            <Route path='notificaciones' element={<Notificaciones/>}/>
-            <Route path='configuracion' element={<Configuracion/>}/>
-
+          {/* Panel usuario */}
+          <Route path='/usuario' element={<UsuarioLayout />}>
+            <Route index element={<DashboardAdmin />} />
+            <Route path='perfil' element={<Perfil />} />
+            <Route path='rutinas' element={<Rutinas />} />
+            <Route path='historial' element={<Historial />} />
+            <Route path='contenido' element={<Contenido />} />
+            <Route path='progreso' element={<Progreso />} />
+            <Route path='analisis' element={<Analisis />} />
+            <Route path='notificaciones' element={<Notificaciones />} />
+            <Route path='configuracion' element={<Configuracion />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
 
-          {/*Panel entrenador */}
-          <Route path='/entrenador' element={<EntrenadorLayout/>}>
-            <Route index element={<Dashboard/>} />
-            <Route path="/perfil" element={<Perfil />} />
-            <Route path="/gestion" element={<Gestion />} />
-            <Route path="/horario" element={<Horario />} />
-            <Route path="/estadisticas" element={<Estadisticas />} />
-            <Route path="/rutinas" element={<Rutinas />} />
-            <Route path="/reportes" element={<Reportes />} />
-            <Route path="/analisis" element={<Analisis />} />
-            <Route path="/configuracion" element={<Configuracion />} />
+          {/* Panel entrenador */}
+          <Route path='/entrenador' element={<EntrenadorLayout />}>
+            <Route index element={<DashboardEntrenador />} />
+            <Route path='perfil' element={<Perfil />} />
+            <Route path='gestion' element={<Gestion />} />
+            <Route path='horario' element={<Horario />} />
+            <Route path='estadisticas' element={<Estadisticas />} />
+            <Route path='rutinas' element={<Rutinas />} />
+            <Route path='reportes' element={<Reportes />} />
+            <Route path='analisis' element={<Analisis />} />
+            <Route path='configuracion' element={<Configuracion />} />
           </Route>
 
-          
         </Routes>
       </div>
     </BrowserRouter>
